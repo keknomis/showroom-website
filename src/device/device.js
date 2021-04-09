64ddc9c11eb98b3e22ab263365ecbfa6893a3e7e
Simon <simon.serbetar@equaleyes.com>
2021-04-10 00:40:21 +0200

    updated work

---
+++ src/device/device.js
@@ -0,0 +1,20 @@
+// import App from "../App"
+import './device.css'
+function Device(){
+    return (
+        <div>
+            <section className='box'>
+            <h1>Sensor name</h1>
+            <img src='#'></img>
+            <h4>Sensor description</h4>
+            <p>Description of what sensor actualy does
+                yada yada yada
+            </p>
+            <button>Read more</button>
+            <button>Goto sensor page</button>
+            </section>
+        </div>
+    )
+}
+
+export default Device
