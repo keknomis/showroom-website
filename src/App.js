b07796f0f8bda972f01b22a40b154a1c587fb8c3
simon <simon.serbetar1@gmail.com>
2021-04-24 20:12:53 +0200

    /

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import data from './device/data';
 
 function App() {
 
-  const [sensors, setSensors]=useState(data)
+    const [sensors, setSensors] = useState(data)
 
-  return (
-      <div>
-    <Device sensors={sensors} />
-    <button onClick={()=>setSensors([])}>Clear </button>
-    </div>
-  )
+    return (
+        <section>
+        <div>
+            <Device sensors={sensors}/>
+        </div>
+        <div>
+          <button onClick={() => setSensors([])}>Clear</button>
+        </div>
+        </section>
+    )
 }
 
 export default App;
